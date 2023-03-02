6aac54cd50b114a0aef8d7fe3987e75d1db3f141
Cassio Santana <cassioo.roger@gmail.com>
2023-03-02 11:26:00 -0300

    Método busca por elemento adicionado

--- src/com/cassio/estruturadados/vetor/teste/Aula06.java
+++ src/com/cassio/estruturadados/vetor/teste/Aula06.java
@@ -2,7 +2,7 @@ package com.cassio.estruturadados.vetor.teste;
 
 import com.cassio.estruturadados.vetor.Vetor;
 
-public class Aula05 {
+public class Aula06 {
     public static void main(String[] args) {
         Vetor vetor = new Vetor(10);
 
@@ -10,6 +10,6 @@ public class Aula05 {
         vetor.adiciona("elemento2"); // posicao 1
         vetor.adiciona("elemento3"); // posicao 2
 
-        System.out.println(vetor.busca(3));
+        System.out.println(vetor.busca("Elemento1"));
     }
 }
